dcb4851f01fc584d26bddf6126d883b3fdc668d9
Ahmed Saad <117980555+baustahmed@users.noreply.github.com>
2023-05-09 19:52:01 +0600

    modified

--- baust/CSE 3202 Artificial Intelligence/Decision Tree/decisionTree.py
+++ baust/CSE 3202 Artificial Intelligence/Decision Tree/decisionTree.py
@@ -1,22 +1,24 @@
+# Import modules:
 import pandas
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
-
+# Read input file:
 df = pandas.read_csv("data.csv")
-
-d = {'UK': 0, 'USA': 1, 'N': 2}
+# Map input file string to numerical:
+d = {'UK': 0, 'USA': 1, 'BD': 2}
 df['Nationality'] = df['Nationality'].map(d)
 d = {'YES': 1, 'NO': 0}
 df['Go'] = df['Go'].map(d)
-
+# Make list of features:
 features = ['Age', 'Experience', 'Rank', 'Nationality']
-
+# Assign features and condition into axis:
 X = df[features]
 y = df['Go']
-
+# Call function for classification:
 dtree = DecisionTreeClassifier()
+# Plot result into X and Y:
 dtree = dtree.fit(X, y)
-
+# Plot tree and show output:
 tree.plot_tree(dtree, feature_names=features)
 plt.show()
